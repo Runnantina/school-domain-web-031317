40f7d2e0e3ee808e20bace50b049cd528dce5f04
Runnan Zhong <r.tina.zhong@gmail.com>
2017-03-16 22:46:35 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,40 @@
 # code here!
+require 'pry'
+
+class School
+
+  attr_accessor :school, :grade, :student
+  attr_reader :roster
+
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+
+  def add_student(student, grade)
+    if !@roster[grade]
+      @roster[grade] = []
+    end
+    @roster[grade] << student
+
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |k,array|
+      @roster[k]=array.sort
+      @roster
+    end
+    # after sorted array, iterate back to hash
+    # set array to values
+      # this will iterate over each key, value pair
+    end
+
+
+
+end
